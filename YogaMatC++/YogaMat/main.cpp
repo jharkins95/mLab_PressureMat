dae45ab21ef00a08993805e5df88e2af001e6dab
kevinvolkel <kevin.volkel@wilkes.edu>
2016-08-21 11:50:37 -0400

    Add files via upload

--- YogaMatC++/YogaMat/main.cpp
+++ YogaMatC++/YogaMat/main.cpp
@@ -9,7 +9,7 @@
 #include <windows.h>
 #include <conio.h>
 
-
+long scan=1;
 using namespace std;
 // serial handle 
 HANDLE serialh;
@@ -28,28 +28,27 @@ int main() {
 	GetCommState(serialh, &serialparams);
 	serialparams.BaudRate = 921600;
 	serialparams.ByteSize = 8;
-	serialparams.StopBits = 2;
+	serialparams.StopBits = 1;
 	serialparams.Parity = 0;
 	SetCommState(serialh, &serialparams);
 	char start = '>';
 	WriteFile(serialh, &start, 1, NULL, NULL);
 	while (1) {
-
+		
 		//wait for the character to tell pc to start reading
 		while (rec != 's') ReadFile(serialh, &rec, 1, NULL, NULL);
-		WriteFile(serialh, &okaychar, 1, NULL, NULL);
+		
 		for (char i = 0; i < 2; i++) {
 			//receicve the data for each element
 			for (char j = 0; j < 4; j++) {
-				WriteFile(serialh, &okaychar, 1, NULL, NULL);
 				while (!ReadFile(serialh, &rec, 1, NULL, NULL));
 				sensordata[recrow][j] = rec;
 				sensordata[recrow][j] = sensordata[recrow][j] << 8;
 				//tell mbed that message was received
-				WriteFile(serialh, &okaychar, 1, NULL, NULL);
+				
 				while (!ReadFile(serialh, &rec, 1, NULL, NULL));
 				sensordata[recrow][j] = rec + sensordata[recrow][j];
-				WriteFile(serialh, &okaychar, 1, NULL, NULL);
+				
 			}
 			recrow++;
 			if (recrow >= 4) recrow = 0;
@@ -60,10 +59,13 @@ int main() {
 		//fill up text file
 		for (char i = 0; i < 2; i++) {
 			//write the two new lines of data
-			datalog <<writerow<<"\t\t"<< linecounter << "\t\t" << sensordata[writerow][0] << "\t\t" << sensordata[writerow][1] << "\t\t" << sensordata[writerow][2] << "\t\t" << sensordata[writerow][3] << '\n';
+			datalog <<scan<<"\t\t"<< writerow<< "\t\t" << sensordata[writerow][0] << "\t\t" << sensordata[writerow][1] << "\t\t" << sensordata[writerow][2] << "\t\t" << sensordata[writerow][3] << '\n';
 			linecounter++;
 			writerow++;
-			if (writerow >= 4) writerow = 0;
+			if (writerow >= 4) {
+				writerow = 0;
+				scan++;
+			}
 		}
 		if (linecounter%10000==0) {
 			datalog.close();
